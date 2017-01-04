73271ef24cb80c9315db269882ccca494ad27fa3
Alexander Nasonov <alnsn@yandex.ru>
2017-01-04 21:50:53 +0000

    Don't change services.sync.prefs.sync.*.

--- www/firefox/patch-browser_app_profile_firefox.js
+++ www/firefox/patch-browser_app_profile_firefox.js
@@ -1,7 +1,7 @@
 Disable auto updates.
 
 --- ./browser/app/profile/firefox.js.orig	2017-01-04 20:25:55.031642372 +0000
-+++ browser/app/profile/firefox.js	2017-01-04 20:31:41.067717307 +0000
++++ ./browser/app/profile/firefox.js	2017-01-04 20:31:41.067717307 +0000
 @@ -50,7 +50,7 @@
  pref("extensions.getAddons.recommended.url", "https://services.addons.mozilla.org/%LOCALE%/%APP%/api/%API_VERSION%/list/recommended/all/%MAX_RESULTS%/%OS%/%VERSION%?src=firefox");
  pref("extensions.getAddons.link.url", "https://addons.mozilla.org/%LOCALE%/firefox/");
@@ -35,21 +35,3 @@ Disable auto updates.
  pref("extensions.update.url", "https://versioncheck.addons.mozilla.org/update/VersionCheck.php?reqVersion=%REQ_VERSION%&id=%ITEM_ID%&version=%ITEM_VERSION%&maxAppVersion=%ITEM_MAXAPPVERSION%&status=%ITEM_STATUS%&appID=%APP_ID%&appVersion=%APP_VERSION%&appOS=%APP_OS%&appABI=%APP_ABI%&locale=%APP_LOCALE%&currentAppVersion=%CURRENT_APP_VERSION%&updateType=%UPDATE_TYPE%&compatMode=%COMPATIBILITY_MODE%");
  pref("extensions.update.background.url", "https://versioncheck-bg.addons.mozilla.org/update/VersionCheck.php?reqVersion=%REQ_VERSION%&id=%ITEM_ID%&version=%ITEM_VERSION%&maxAppVersion=%ITEM_MAXAPPVERSION%&status=%ITEM_STATUS%&appID=%APP_ID%&appVersion=%APP_VERSION%&appOS=%APP_OS%&appABI=%APP_ABI%&locale=%APP_LOCALE%&currentAppVersion=%CURRENT_APP_VERSION%&updateType=%UPDATE_TYPE%&compatMode=%COMPATIBILITY_MODE%");
  pref("extensions.update.interval", 86400);  // Check for updates to Extensions and
-@@ -1042,7 +1042,7 @@
- pref("services.sync.prefs.sync.browser.offline-apps.notify", true);
- pref("services.sync.prefs.sync.browser.safebrowsing.phishing.enabled", true);
- pref("services.sync.prefs.sync.browser.safebrowsing.malware.enabled", true);
--pref("services.sync.prefs.sync.browser.search.update", true);
-+pref("services.sync.prefs.sync.browser.search.update", false);
- pref("services.sync.prefs.sync.browser.sessionstore.restore_on_demand", true);
- pref("services.sync.prefs.sync.browser.startup.homepage", true);
- pref("services.sync.prefs.sync.browser.startup.page", true);
-@@ -1061,7 +1061,7 @@
- pref("services.sync.prefs.sync.dom.disable_window_move_resize", true);
- pref("services.sync.prefs.sync.dom.event.contextmenu.enabled", true);
- pref("services.sync.prefs.sync.extensions.personas.current", true);
--pref("services.sync.prefs.sync.extensions.update.enabled", true);
-+pref("services.sync.prefs.sync.extensions.update.enabled", false);
- pref("services.sync.prefs.sync.intl.accept_languages", true);
- pref("services.sync.prefs.sync.javascript.enabled", true);
- pref("services.sync.prefs.sync.layout.spellcheckDefault", true);

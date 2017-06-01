17c0809b740e515f61fc7c86051fbe919668687b
Alexander Nasonov <alnsn@yandex.ru>
2017-06-01 20:50:22 +0100

    Update patches to the latest security/tor-browser version.

--- security/tor-browser/patch-browser_app_profile_firefox.js
+++ security/tor-browser/patch-browser_app_profile_firefox.js
@@ -1,8 +1,8 @@
 Disable auto updates. Open about:blank on startup.
 
---- browser/app/profile/firefox.js.orig	2017-01-04 23:28:36.963107339 +0000
-+++ browser/app/profile/firefox.js	2017-01-04 23:29:32.641085000 +0000
-@@ -57,7 +57,7 @@
+--- browser/app/profile/firefox.js.orig	2016-12-10 08:02:57.000000000 +0000
++++ browser/app/profile/firefox.js
+@@ -62,7 +62,7 @@ pref("extensions.blocklist.url", "https:
  pref("extensions.blocklist.detailsURL", "https://www.mozilla.org/%LOCALE%/blocklist/");
  pref("extensions.blocklist.itemURL", "https://blocklist.addons.mozilla.org/%LOCALE%/%APP%/blocked/%blockID%");
  
@@ -11,7 +11,7 @@ Disable auto updates. Open about:blank on startup.
  
  pref("extensions.hotfix.id", "firefox-hotfix@mozilla.org");
  pref("extensions.hotfix.cert.checkAttributes", true);
-@@ -108,12 +108,12 @@
+@@ -121,12 +121,12 @@ pref("app.update.cert.requireBuiltIn", f
  pref("app.update.cert.checkAttributes", false);
  
  // Whether or not app updates are enabled
@@ -25,8 +25,8 @@ Disable auto updates. Open about:blank on startup.
 +pref("app.update.auto", false);
  
  // See chart in nsUpdateService.js source for more details
- // incompatibilities are ignored by updates in Metro
-@@ -180,7 +180,7 @@
+ pref("app.update.mode", 1);
+@@ -186,7 +186,7 @@ pref("app.update.service.enabled", true)
  //  extensions.{GUID}.update.url
  //  .. etc ..
  //
@@ -34,17 +34,17 @@ Disable auto updates. Open about:blank on startup.
 +pref("extensions.update.enabled", false);
  pref("extensions.update.url", "https://versioncheck.addons.mozilla.org/update/VersionCheck.php?reqVersion=%REQ_VERSION%&id=%ITEM_ID%&version=%ITEM_VERSION%&maxAppVersion=%ITEM_MAXAPPVERSION%&status=%ITEM_STATUS%&appID=%APP_ID%&appVersion=%APP_VERSION%&appOS=%APP_OS%&appABI=%APP_ABI%&locale=%APP_LOCALE%&currentAppVersion=%CURRENT_APP_VERSION%&updateType=%UPDATE_TYPE%&compatMode=%COMPATIBILITY_MODE%");
  pref("extensions.update.background.url", "https://versioncheck-bg.addons.mozilla.org/update/VersionCheck.php?reqVersion=%REQ_VERSION%&id=%ITEM_ID%&version=%ITEM_VERSION%&maxAppVersion=%ITEM_MAXAPPVERSION%&status=%ITEM_STATUS%&appID=%APP_ID%&appVersion=%APP_VERSION%&appOS=%APP_OS%&appABI=%APP_ABI%&locale=%APP_LOCALE%&currentAppVersion=%CURRENT_APP_VERSION%&updateType=%UPDATE_TYPE%&compatMode=%COMPATIBILITY_MODE%");
- pref("extensions.update.interval", 86400);  // Check for updates to Extensions and 
-@@ -193,7 +193,7 @@
+ pref("extensions.update.interval", 86400);  // Check for updates to Extensions and
+@@ -199,7 +199,7 @@ pref("extensions.dss.switchPending", fal
  pref("extensions.{972ce4c6-7e08-4474-a285-3208198ce6fd}.name", "chrome://browser/locale/browser.properties");
  pref("extensions.{972ce4c6-7e08-4474-a285-3208198ce6fd}.description", "chrome://browser/locale/browser.properties");
  
 -pref("lightweightThemes.update.enabled", true);
 +pref("lightweightThemes.update.enabled", false);
  pref("lightweightThemes.getMoreURL", "https://addons.mozilla.org/%LOCALE%/firefox/themes");
- pref("lightweightThemes.recommendedThemes", "[{\"id\":\"recommended-1\",\"homepageURL\":\"https://addons.mozilla.org/firefox/addon/a-web-browser-renaissance/\",\"headerURL\":\"resource:///chrome/browser/content/browser/defaultthemes/1.header.jpg\",\"footerURL\":\"resource:///chrome/browser/content/browser/defaultthemes/1.footer.jpg\",\"textcolor\":\"#000000\",\"accentcolor\":\"#f2d9b1\",\"iconURL\":\"resource:///chrome/browser/content/browser/defaultthemes/1.icon.jpg\",\"previewURL\":\"resource:///chrome/browser/content/browser/defaultthemes/1.preview.jpg\",\"author\":\"Sean.Martell\",\"version\":\"0\"},{\"id\":\"recommended-2\",\"homepageURL\":\"https://addons.mozilla.org/firefox/addon/space-fantasy/\",\"headerURL\":\"resource:///chrome/browser/content/browser/defaultthemes/2.header.jpg\",\"footerURL\":\"resource:///chrome/browser/content/browser/defaultthemes/2.footer.jpg\",\"textcolor\":\"#ffffff\",\"accentcolor\":\"#d9d9d9\",\"iconURL\":\"resource:///chrome/browser/content/browser/defaultthemes/2.icon.jpg\",\"previewURL\":\"resource:///chrome/browser/content/browser/defaultthemes/2.preview.jpg\",\"author\":\"fx5800p\",\"version\":\"1.0\"},{\"id\":\"recommended-3\",\"homepageURL\":\"https://addons.mozilla.org/firefox/addon/linen-light/\",\"headerURL\":\"resource:///chrome/browser/content/browser/defaultthemes/3.header.png\",\"footerURL\":\"resource:///chrome/browser/content/browser/defaultthemes/3.footer.png\",\"textcolor\":\"#None\",\"accentcolor\":\"#ada8a8\",\"iconURL\":\"resource:///chrome/browser/content/browser/defaultthemes/3.icon.png\",\"previewURL\":\"resource:///chrome/browser/content/browser/defaultthemes/3.preview.png\",\"author\":\"DVemer\",\"version\":\"1.0\"},{\"id\":\"recommended-4\",\"homepageURL\":\"https://addons.mozilla.org/firefox/addon/pastel-gradient/\",\"headerURL\":\"resource:///chrome/browser/content/browser/defaultthemes/4.header.png\",\"footerURL\":\"resource:///chrome/browser/content/browser/defaultthemes/4.footer.png\",\"textcolor\":\"#000000\",\"accentcolor\":\"#000000\",\"iconURL\":\"resource:///chrome/browser/content/browser/defaultthemes/4.icon.png\",\"previewURL\":\"resource:///chrome/browser/content/browser/defaultthemes/4.preview.png\",\"author\":\"darrinhenein\",\"version\":\"1.0\"},{\"id\":\"recommended-5\",\"homepageURL\":\"https://addons.mozilla.org/firefox/addon/carbon-light/\",\"headerURL\":\"resource:///chrome/browser/content/browser/defaultthemes/5.header.png\",\"footerURL\":\"resource:///chrome/browser/content/browser/defaultthemes/5.footer.png\",\"textcolor\":\"#3b3b3b\",\"accentcolor\":\"#2e2e2e\",\"iconURL\":\"resource:///chrome/browser/content/browser/defaultthemes/5.icon.jpg\",\"previewURL\":\"resource:///chrome/browser/content/browser/defaultthemes/5.preview.jpg\",\"author\":\"Jaxivo\",\"version\":\"1.0\"}]");
+ pref("lightweightThemes.recommendedThemes", "[{\"id\":\"recommended-1\",\"homepageURL\":\"https://addons.mozilla.org/firefox/addon/a-web-browser-renaissance/\",\"headerURL\":\"resource:///chrome/browser/content/browser/defaultthemes/1.header.jpg\",\"footerURL\":\"resource:///chrome/browser/content/browser/defaultthemes/1.footer.jpg\",\"textcolor\":\"#000000\",\"accentcolor\":\"#f2d9b1\",\"iconURL\":\"resource:///chrome/browser/content/browser/defaultthemes/1.icon.jpg\",\"previewURL\":\"resource:///chrome/browser/content/browser/defaultthemes/1.preview.jpg\",\"author\":\"Sean.Martell\",\"version\":\"0\"},{\"id\":\"recommended-2\",\"homepageURL\":\"https://addons.mozilla.org/firefox/addon/space-fantasy/\",\"headerURL\":\"resource:///chrome/browser/content/browser/defaultthemes/2.header.jpg\",\"footerURL\":\"resource:///chrome/browser/content/browser/defaultthemes/2.footer.jpg\",\"textcolor\":\"#ffffff\",\"accentcolor\":\"#d9d9d9\",\"iconURL\":\"resource:///chrome/browser/content/browser/defaultthemes/2.icon.jpg\",\"previewURL\":\"resource:///chrome/browser/content/browser/defaultthemes/2.preview.jpg\",\"author\":\"fx5800p\",\"version\":\"1.0\"},{\"id\":\"recommended-3\",\"homepageURL\":\"https://addons.mozilla.org/firefox/addon/linen-light/\",\"headerURL\":\"resource:///chrome/browser/content/browser/defaultthemes/3.header.png\",\"footerURL\":\"resource:///chrome/browser/content/browser/defaultthemes/3.footer.png\",\"accentcolor\":\"#ada8a8\",\"iconURL\":\"resource:///chrome/browser/content/browser/defaultthemes/3.icon.png\",\"previewURL\":\"resource:///chrome/browser/content/browser/defaultthemes/3.preview.png\",\"author\":\"DVemer\",\"version\":\"1.0\"},{\"id\":\"recommended-4\",\"homepageURL\":\"https://addons.mozilla.org/firefox/addon/pastel-gradient/\",\"headerURL\":\"resource:///chrome/browser/content/browser/defaultthemes/4.header.png\",\"footerURL\":\"resource:///chrome/browser/content/browser/defaultthemes/4.footer.png\",\"textcolor\":\"#000000\",\"accentcolor\":\"#000000\",\"iconURL\":\"resource:///chrome/browser/content/browser/defaultthemes/4.icon.png\",\"previewURL\":\"resource:///chrome/browser/content/browser/defaultthemes/4.preview.png\",\"author\":\"darrinhenein\",\"version\":\"1.0\"},{\"id\":\"recommended-5\",\"homepageURL\":\"https://addons.mozilla.org/firefox/addon/carbon-light/\",\"headerURL\":\"resource:///chrome/browser/content/browser/defaultthemes/5.header.png\",\"footerURL\":\"resource:///chrome/browser/content/browser/defaultthemes/5.footer.png\",\"textcolor\":\"#3b3b3b\",\"accentcolor\":\"#2e2e2e\",\"iconURL\":\"resource:///chrome/browser/content/browser/defaultthemes/5.icon.jpg\",\"previewURL\":\"resource:///chrome/browser/content/browser/defaultthemes/5.preview.jpg\",\"author\":\"Jaxivo\",\"version\":\"1.0\"}]");
  
-@@ -228,8 +228,8 @@
+@@ -239,8 +239,8 @@ pref("browser.shell.defaultBrowserCheckC
  
  // 0 = blank, 1 = home (browser.startup.homepage), 2 = last visited page, 3 = resume previous browser session
  // The behavior of option 3 is detailed at: http://wiki.mozilla.org/Session_Restore
@@ -55,7 +55,7 @@ Disable auto updates. Open about:blank on startup.
  
  pref("browser.slowStartup.notificationDisabled", false);
  pref("browser.slowStartup.timeThreshold", 40000);
-@@ -1239,7 +1239,7 @@
+@@ -1268,7 +1268,7 @@ pref("services.sync.prefs.sync.browser.n
  pref("services.sync.prefs.sync.browser.offline-apps.notify", true);
  pref("services.sync.prefs.sync.browser.safebrowsing.enabled", true);
  pref("services.sync.prefs.sync.browser.safebrowsing.malware.enabled", true);
--- security/tor-browser/patch-modules_libpref_init_all.js
+++ security/tor-browser/patch-modules_libpref_init_all.js
@@ -1,8 +1,8 @@
-Set sock proxy to 127.0.0.1:9050. Do not save passwords.
+Set sock proxy to 127.0.0.1:9150. Do not save passwords.
 
---- modules/libpref/init/all.js.orig	2017-01-04 23:31:26.940059178 +0000
-+++ modules/libpref/init/all.js	2017-01-04 23:34:23.468152422 +0000
-@@ -1672,17 +1672,17 @@
+--- modules/libpref/init/all.js.orig	2016-12-10 08:02:57.000000000 +0000
++++ modules/libpref/init/all.js
+@@ -1837,17 +1837,17 @@
  
  pref("permissions.default.image",           1); // 1-Accept, 2-Deny, 3-dontAcceptForeign
  
@@ -17,21 +17,26 @@ Set sock proxy to 127.0.0.1:9050. Do not save passwords.
 -pref("network.proxy.socks",                 "");
 -pref("network.proxy.socks_port",            0);
 +pref("network.proxy.socks",                 "127.0.0.1");
-+pref("network.proxy.socks_port",            9050);
++pref("network.proxy.socks_port",            9150);
  pref("network.proxy.socks_version",         5);
 -pref("network.proxy.socks_remote_dns",      false);
 +pref("network.proxy.socks_remote_dns",      true);
  pref("network.proxy.proxy_over_tls",        true);
  pref("network.proxy.no_proxies_on",         "localhost, 127.0.0.1");
  pref("network.proxy.failover_timeout",      1800); // 30 minutes
-@@ -3708,8 +3708,8 @@
+@@ -4093,13 +4093,13 @@
  #endif
  
  // Login Manager prefs
 -pref("signon.rememberSignons",              true);
--pref("signon.autofillForms",                true);
 +pref("signon.rememberSignons",              false);
+ #ifdef NIGHTLY_BUILD
+ pref("signon.rememberSignons.visibilityToggle", true);
+ #else
+ pref("signon.rememberSignons.visibilityToggle", false);
+ #endif
+-pref("signon.autofillForms",                true);
 +pref("signon.autofillForms",                false);
  pref("signon.autologin.proxy",              false);
  pref("signon.storeWhenAutocompleteOff",     true);
- pref("signon.debug",                        false);
+ pref("signon.ui.experimental",              false);
